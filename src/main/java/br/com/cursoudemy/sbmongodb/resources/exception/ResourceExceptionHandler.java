8521e1412ca0d0eeb87b4505e13dbfcbf1b89c43
gabriel <gabrielmqs04@gmail.com>
2018-09-12 22:19:17 -0300

    aula 208 - busca de usuário por id e tratamento de exceptions personalizada
    adicionando classe manipuladora de exception de resources
    adicionando classe para personalizar erro padrão do backend
    adicionando implementação do metodo findById na UserResource
    adicionando implementação do metodo findById na UserService
    adicionando exception personalizada

---
+++ src/main/java/br/com/cursoudemy/sbmongodb/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package br.com.cursoudemy.sbmongodb.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.cursoudemy.sbmongodb.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError error = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+	}
+}
