0cd61f426cd242d2a8b7e584834fbfb597699a20
gabriel <gabrielmqs04@gmail.com>
2018-09-14 01:26:55 -0300

    aula 214 - criando AuthorDTO e refatorando as classes Post e Instantiation
    refatorando a classe responsável pela carga inicial da base de dados para usar AuthorDTO
    refatorando a classe Post alterando o tipo do atibuto author para AuthorDTO
    adicionando implementação do dto AuthorDTO

---
+++ src/main/java/br/com/cursoudemy/sbmongodb/dto/AuthorDTO.java
@@ -0,0 +1,35 @@
+package br.com.cursoudemy.sbmongodb.dto;
+
+import java.io.Serializable;
+
+import br.com.cursoudemy.sbmongodb.domain.User;
+
+public class AuthorDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String name;
+	
+	public AuthorDTO() { }
+	
+	public AuthorDTO(User user) {
+		this.id = user.getId();
+		this.name = user.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
