b8cd23bdbd8018703458d633608b891b7b70370a
gabriel <gabrielmqs04@gmail.com>
2018-09-15 01:26:07 -0300

    aula 219 - Consulta simples com query methods
    atualizando PostResource implementando método que busca os posts pelo título
    adicionando implementação da classe utilitária URL com um método para decodificar parâmetros de url
    adicionando método de busca por título do post no serviço PostService
    adicionando método de busca por título do post no repositorio PostRepository

--- src/main/java/br/com/cursoudemy/sbmongodb/resources/PostResource.java
+++ src/main/java/br/com/cursoudemy/sbmongodb/resources/PostResource.java
@@ -1,13 +1,17 @@
 package br.com.cursoudemy.sbmongodb.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cursoudemy.sbmongodb.domain.Post;
+import br.com.cursoudemy.sbmongodb.resources.util.URL;
 import br.com.cursoudemy.sbmongodb.services.PostService;
 
 @RestController
@@ -21,5 +25,12 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String postId) {
 		Post post = service.findById(postId);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text); // decodifica o parametro enviado no  request
+		List<Post> posts = service.findByTitle(text);
+		return ResponseEntity.ok().body(posts);
 	}
 }
